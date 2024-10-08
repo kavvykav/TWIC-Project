dac44e9cec36a5299174cf9c4fb52c19d02196c8
Jakob Delaney <105943291+Jadyin@users.noreply.github.com>
2024-10-08 19:12:12 -0400

    Update main.rs

--- docs/prototypes/threads-prototype/src/main.rs
+++ docs/prototypes/threads-prototype/src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let (tx1, rx1) = mpsc::channel();  // person -> machine
     let (tx2, rx2) = mpsc::channel();  // machine -> person
 
-    // Spawn the sender (person.rs) thread
+    // Spawn person thread
     let sender = thread::spawn(move || {
         person::send_values(tx1, rx2);
     });
 
-    // Spawn the receiver (machine.rs) thread
+    // Spawn machine thread
     let receiver = thread::spawn(move || {
         machine::receive_values(rx1, tx2);
     });
