88541fdb4470f77a4454f2b7c91b0665c158e1b7
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-16 17:10:12 -0500

    found my mistake after testing it lol

--- src/database/test_db.sh
+++ src/database/test_db.sh
@@ -13,4 +13,4 @@ echo '{"command": "UPDATE", "checkpoint_id": 1, "worker_id": 1, "worker_name": "
 echo ""
 echo '{"command": "AUTHENTICATE", "checkpoint_id": 1, "worker_id": 1, "worker_name": "Bob", "worker_fingerprint": "this_is_a_hash", "location": "Halifax", "authorized_roles": "janitor", "role_id": 1}' | nc 127.0.0.1 3036
 echo ""
-echo '{"command": "DELETE", "data": "1"}' | nc 127.0.0.1 3036
+echo '{"command": "DELETE", "checkpoint_id": 1, "worker_id": 1, "worker_name": "Bob", "worker_fingerprint": "this_is_a_hash", "location": "Halifax", "authorized_roles": "janitor", "role_id": 1}' | nc 127.0.0.1 3036
