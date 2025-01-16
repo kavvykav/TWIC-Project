6c2028856c068571568c135a83c92c642458067f
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-16 17:07:05 -0500

    put new data structure for requests in test script (didn't test yet)

--- src/database/test_db.sh
+++ src/database/test_db.sh
@@ -5,12 +5,12 @@ if ! which nc &>/dev/null; then
 	exit 1
 fi
 
-echo '{"command": "ENROLL", "data": "Alice Brown,Port F,Manager"}' | nc 127.0.0.1 3036
+echo '{"command": "ENROLL", "checkpoint_id": 1, "worker_id": 1, "worker_name": "Bob", "worker_fingerprint": "this_is_a_hash", "location": "Halifax", "authorized_roles": "janitor", "role_id": 1}' | nc 127.0.0.1 3036
 echo ""
-echo '{"command": "AUTHENTICATE", "data": "1"}' | nc 127.0.0.1 3036
+echo '{"command": "AUTHENTICATE", "checkpoint_id": 1, "worker_id": 1, "worker_name": "Bob", "worker_fingerprint": "this_is_a_hash", "location": "Halifax", "authorized_roles": "janitor", "role_id": 1}' | nc 127.0.0.1 3036
 echo ""
-echo '{"command": "UPDATE", "data": "1,Admin"}' | nc 127.0.0.1 3036
+echo '{"command": "UPDATE", "checkpoint_id": 1, "worker_id": 1, "worker_name": "Bob", "worker_fingerprint": "this_is_a_hash", "location": "Halifax", "authorized_roles": "janitor", "role_id": 2}' | nc 127.0.0.1 3036
 echo ""
-echo '{"command": "AUTHENTICATE", "data": "1"}' | nc 127.0.0.1 3036
+echo '{"command": "AUTHENTICATE", "checkpoint_id": 1, "worker_id": 1, "worker_name": "Bob", "worker_fingerprint": "this_is_a_hash", "location": "Halifax", "authorized_roles": "janitor", "role_id": 1}' | nc 127.0.0.1 3036
 echo ""
 echo '{"command": "DELETE", "data": "1"}' | nc 127.0.0.1 3036
